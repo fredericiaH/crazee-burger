4d16f984e6c7f7285bd45c91b8ce20106d2993bb
fredericiaH <elsa.heron@gmail.com>
2024-05-31 14:40:40 -0400

    ✅ F01: Login >create LoginPage

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function LoginPage(){
+    //States
+
+    const [name, setName] = useState("");
+
+    //Comportements
+
+    const handleSubmit = (event) => {
+        alert("Bonjour " + `${name}`);
+        setName("");
+        
+    }
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        setName(event.target.value);
+        
+    }
+    //Affichage
+
+
+    return(
+        <form action="submit" onSubmit={handleSubmit}>
+            <h1>Bienvenue chez nous</h1>
+            <h2>Connectez-vous</h2>
+            <br/>
+            <input type="text" placeholder="Entrez votre prenom..." onChange={handleChange} required />
+            <button>Acceder à votre espace</button>
+
+        </form>
+
+    )
+}
