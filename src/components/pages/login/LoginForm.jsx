02af9c6a80fa1b9a960617240fc136dcee7c9d88
fredericiaH <elsa.heron@gmail.com>
2024-08-11 11:04:05 -0400

    💄 F03: LOGIN > LoginForm > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import OrderPage from "../order/OrderPage";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
+import { theme } from "../../theme";
 
 
 export default function LoginForm(){
@@ -30,12 +31,12 @@ export default function LoginForm(){
 
     return(
         <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-            <h1>Bienvenue chez nous</h1>
+            <h1>Bienvenue chez nous !</h1>
+            <hr/>
             <h2>Connectez-vous</h2>
-            <br/>
             <input type="text" placeholder="Entrez votre prenom..." onChange={handleChange} required />
                 
-            <button>Acceder à votre espace</button>
+            <button>Acceder à mon espace</button>
         </LoginFormStyled>
     )
     
@@ -43,7 +44,53 @@ export default function LoginForm(){
 
 const LoginFormStyled = styled.form`
     background: green;
-    /*display: flex;
-    flex-direction: column;*/
+    text-align: center;
+    max-width: 500px;
+    min-width: 400px;
+    margin: 0px auto;
+    padding: 2.5rem 2rem;
+    border-radius: 5px;
+    font-family: "Amatic SC", cursive;
+    
+    
+    
+
+    h2{
+        color: #8e8b8b;
+        margin: 20px 10px 10px;
+        color: white;
+        font-size: 36px;
+        
+    }
+
+    h1{
+        color: white;
+        font-size: 47px;
+    }
+    
+
+    hr{
+        border: 1.5px solid ${theme.colors.redSecondary};
+        margin-bottom: 40px;
+    }
+
+    input{
+        width: 73%;
+        height: 40px;
+        padding-left: 10%;
+        padding-right: 2%;
+        border-radius: ${theme.borderRadius.round};
+    }
+
+    button{
+        color: ${theme.colors.white};
+        background-color: ${theme.colors.primary_burger};
+        margin-top: 10px;
+        width: 86%;
+        height: 45px;
+        border-radius: ${theme.borderRadius.round};
+    }
+
+    
 
 `
