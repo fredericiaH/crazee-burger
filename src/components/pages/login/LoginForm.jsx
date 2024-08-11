9204c9ba6198ac497fbd4d0f9a137da2c54b3869
fredericiaH <elsa.heron@gmail.com>
2024-08-11 11:57:52 -0400

    💄  F03: LOGIN > LoginForm > input >Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -3,6 +3,8 @@ import OrderPage from "../order/OrderPage";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import { theme } from "../../theme";
+import { BsPersonCircle } from "react-icons/bs";
+
 
 
 export default function LoginForm(){
@@ -34,8 +36,10 @@ export default function LoginForm(){
             <h1>Bienvenue chez nous !</h1>
             <hr/>
             <h2>Connectez-vous</h2>
-            <input type="text" placeholder="Entrez votre prenom..." onChange={handleChange} required />
-                
+            <div className="input-with-icon">
+                <BsPersonCircle className="icon" />
+                <input type="text" placeholder="Entrez votre prenom..." onChange={handleChange} required />
+            </div>   
             <button>Acceder à mon espace</button>
         </LoginFormStyled>
     )
@@ -75,11 +79,10 @@ const LoginFormStyled = styled.form`
     }
 
     input{
-        width: 73%;
-        height: 40px;
-        padding-left: 10%;
-        padding-right: 2%;
-        border-radius: ${theme.borderRadius.round};
+        border: none;
+        font-size: 15px;
+        color: #17161a;
+        
     }
 
     button{
@@ -90,6 +93,27 @@ const LoginFormStyled = styled.form`
         height: 45px;
         border-radius: ${theme.borderRadius.round};
     }
+
+    .input-with-icon{
+        background-color: #fff;
+        border-radius: 5px;
+        display: flex;
+        align-items: center;
+        padding: 18px 24px;
+        margin: 18px 0;
+    }
+
+    .icon{
+        font-size: 15px;
+        margin-right: 8px;
+        color: #93a2b1;
+    }
+
+    /*&::placeholder{
+        background: white;
+        color: lightgrey;
+    }*/
+    
 
     
 
