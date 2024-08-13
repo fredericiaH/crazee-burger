f4028dfbd6690bfdd19b970f2770442f40a8b286
fredericiaH <elsa.heron@gmail.com>
2024-08-13 14:55:47 -0400

    💄 F03: UPDATE design system

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -29,17 +29,27 @@ const colors = {
   }
   
   const fonts = {
-    XXXS: "8px",
-    XXS: "10px",
-    XS: "12px",
-    P0: "15px",
-    P1: "18px",
-    P2: "20px",
-    P3: "24px",
-    P4: "36px",
-    P5: "48px",
-    P6: "60px",
-  }
+    size: {
+      XXXS: "8px",
+      XXS: "10px",
+      XS: "12px",
+      P0: "15px",
+      P1: "18px",
+      P2: "20px",
+      P3: "24px",
+      P4: "36px",
+      P5: "48px",
+      P6: "60px",
+    },
+    weights:{
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700,
+      heavy: 800,
+    },
+}
   const gridUnit = 8
   const borderRadius = {
     subtle: 1,
@@ -48,14 +58,6 @@ const colors = {
     circle: "50%",
   }
   const shadows = {}
-  const weights = {
-    light: 300,
-    regular: 400,
-    medium: 500,
-    semiBold: 600,
-    bold: 700,
-    heavy: 800,
-  }
   
   export const theme = {
     colors,
@@ -63,7 +65,6 @@ const colors = {
     gridUnit,
     borderRadius,
     shadows,
-    weights,
     spacing,
   }
   
