581746a1e103c8b3efda0634b91ddd657ff1075e
fredericiaH <elsa.heron@gmail.com>
2024-08-20 20:28:53 -0400

    🚧  F04: ORDER > UPDATE Orderpage

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,18 +1,54 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
+import styled from 'styled-components';
+import Navbar from './Navbar';
+import Main from './Main';
+//import Nav from '../../reusable-ui/Nav';
 
 
 export default function OrderPage() {
   const {username} = useParams();
-
-  console.log(username);
+  
 
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-      <button>Deconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyled>
+      <div className='contenaire'> 
+        <Navbar username={username}/>
+        {/*<div className='navbar'>navbar</div>*/}
+        {/*<Main />*/}
+        <div className='main'>main</div>
+      </div>
+        
+      
+    </OrderPageStyled>
   )
 }
+
+const OrderPageStyled = styled.div`
+  width: 100%;
+  height: 100%;
+  background: orange;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  
+  
+  
+
+  .contenaire{
+    border: 2px solid red;
+    margin-top: 1%;
+    width: 95%;
+    height: 95%;
+    
+  }
+
+  
+
+  .main{
+    background: green;
+    height: 85%;
+  }
+
+  
+`
