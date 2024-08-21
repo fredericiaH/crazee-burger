c2e2b747df9070fd50b33a1fc399d63cca254086
fredericiaH <elsa.heron@gmail.com>
2024-08-20 20:31:23 -0400

    F04 In progress

---
+++ src/components/pages/order/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Main() {
+  return (
+    <MainStyled>Main</MainStyled>
+  )
+}
+
+const MainStyled = styled.div`
+  background: green;
+  
+`
